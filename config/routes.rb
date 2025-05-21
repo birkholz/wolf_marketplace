4ba983aa18834949d0cf7ca5ca455b90cd469e60
Brandon Birkholz <brandonjbirkholz@gmail.com>
2025-05-21 16:26:14 -0500

    Add the sidekiq web routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
+require "sidekiq/web" # require the web UI
+
 Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  mount Sidekiq::Web => "/sidekiq" # access it at http://localhost:3000/sidekiq
 
   namespace :api do
     # Client authentication
